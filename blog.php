0cecaa87e27aa0ec2344d172fa441e9793a92d1c
DennisDaddy <softteez95@gmail.com>
2016-11-20 14:13:29 -0800

    remove blog item

--- blog.php
+++ blog.php
@@ -131,24 +131,6 @@
                         </div>    
                     </div><!--/.blog-item-->
                         
-                    <div class="blog-item">
-                        <div class="row">
-                             <div class="col-sm-2">
-                                <div class="entry-meta"> 
-                                    <span id="publish_date">07  JUNY</span>
-                                    <span><i class="fa fa-user"></i> <a href="#">John Doe</a></span>
-                                    <span><i class="fa fa-comment"></i> <a href="#">2 Comments</a></span>
-                                    <span><i class="fa fa-heart"></i><a href="#">56 Likes</a></span>
-                                </div>
-                            </div>
-                            <div class="col-sm-10 blog-content">
-                                <a href=""><img class="img-responsive img-blog" src="images/3.jpg" width="100%" alt="" /></a>
-                                <h4>Consequat bibendum quam liquam viverra</h4>
-                                <p>Curabitur quis libero leo, pharetra mattis eros. Praesent consequat libero eget dolor convallis vel rhoncus magna scelerisque. Donec nisl ante, elementum eget posuere a, consectetur a metus. Proin a adipiscing sapien. Suspendisse vehicula porta lectus vel semper. Nullam sapien elit, lacinia eu tristique non.posuere at mi. Morbi at turpis id urna ullamcorper ullamcorper.</p>
-                                <a class="btn btn-primary readmore">Read More <i class="fa fa-angle-right"></i></a>
-                            </div>
-                        </div>    
-                    </div><!--/.blog-item-->
                 </div><!--/.col-md-8-->
 
                 <aside class="col-md-4">
