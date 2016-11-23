4995a84d8d66f624638ca7ff8344dce75e790f7e
DennisDaddy <softteez95@gmail.com>
2016-11-23 11:25:06 -0800

    add community page

---
+++ community.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" dir="ltr" lang="en-US">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" dir="ltr" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" dir="ltr" lang="en-US">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html dir="ltr" lang="en-US">
+    <!--<![endif]-->
+    <head>
+        
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        
+        <title>Usama | Community</title>
+        
+        <!-- [favicon] begin -->
+        <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+        <!-- [favicon] end --> 
+        
+        <!-- CSS Main -->
+        <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 960px)" href="css/lessthen960.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="css/lessthen600.css" />
+        <link rel="stylesheet" type="text/css" media="screen and (max-width: 480px)" href="css/lessthen480.css" />
+        <link rel="stylesheet" type="text/css" media="all" href="css/memento.css"  />
+        
+        <!-- CSS Plugin -->
+        <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/tipsy.css" type="text/css" media="all" />
+        <link rel='stylesheet' href='css/buttons.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/labels.min.css' type='text/css' media='all' />
+		<link rel='stylesheet' href='css/wells.min.css' type='text/css' media='all' />
+        
+        <!-- CSS Slider -->
+        <link rel="stylesheet" href="css/slider-thumbnails.css" type="text/css" media="all" />
+        
+        <link rel="stylesheet" type="text/css" media="all" href="css/homes/default.css" />
+        
+
+        <!-- FONTS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans&amp;subset=latin%2Ccyrillic%2Cgreek&amp;ver=3.4.1" type="text/css" media="all" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Shadows+Into+Light&amp;subset=latin%2Ccyrillic%2Cgreek&amp;ver=3.4.1" type="text/css" media="all" />
+        <link rel='stylesheet' href='http://yourinspirationweb.com/demo/memento/wp-content/themes/memento/core/includes/css/font-awesome.css?ver=3.4.1' type='text/css' media='all' />
+        <link rel='stylesheet' href='http://yourinspirationweb.com/demo/memento/wp-content/themes/memento/fonts/socialico/stylesheet.css?ver=3.4.1' type='text/css' media='all' />
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed%3A300%7CPlayfair+Display%3A400italic&#038;ver=3.4.1' type='text/css' media='all' />
+        
+        <!-- JavaScripts -->
+        <script type="text/javascript">
+            var yiw_prettyphoto_style = 'pp_default';
+        </script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.cycle.min.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+        <script type="text/javascript" src="js/jquery.tipsy.js"></script>
+        <script type="text/javascript" src="js/jquery.tweetable.js"></script>
+        <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+        <script type="text/javascript" src="js/jquery.flexslider.min.js"></script>
+        <script type="text/javascript" src="js/jquery.aw-showcase.js"></script>
+        <script type="text/javascript" src="js/superfish.js"></script>
+        <script type='text/javascript' src="js/jquery.eislideshow.js"></script>
+        <script type='text/javascript' src="js/swfobject.js"></script>
+        <script type='text/javascript' src='js/jquery.cookie.js'></script>
+        <script type='text/javascript' src='js/buttons.min.js'></script>
+        <script type='text/javascript' src='js/layerslider.kreaturamedia.jquery-min.js'></script>
+        <script type='text/javascript' src='js/jquery.quicksand.js'></script>
+        
+    </head>
+    <body class="no_js responsive boxed-layout chrome ">
+        
+        <!-- TOPBAR -->
+
+        <!-- END TOPBAR -->
+        
+        <!-- START WRAPPER -->
+        <div class="wrapper group">
+            <!-- START HEADER -->
+            <div id="header" class="group">
+                <div class="group inner">
+                    
+                    <!-- START LOGO -->
+                    <div id="logo" class="group">
+                       <h2>
+                            <a href="index.php">USAMA MUKWAYA</a>                          
+                        </h2>
+                        <p>Lots of love</p>
+                    </div>
+                    <!-- END LOGO -->  
+                    
+                    <!-- START NAV -->
+                    <div id="nav" class="group">
+                        <ul id="menu-main-nav" class="level-1">
+                            <li>
+                                <a href="index.php"><i class="icon-home"></i>Home</a>
+                                
+                            </li>
+                            
+                                                       
+                            <li>
+                                <a href="osam.php"><i class="icon-picture"></i> Osam</a>
+                                <ul class="sub-menu">
+                                    <li><a href="news.php">News</a></li>
+                                    <li><a href="awards.php">Awards</a></li>
+                                    <li> <a href="blog.php">Blog</a></li>
+                                    <li> <a href="gallery.php">Gallery</a></li>
+                                    <li> <a href="contact.php">Contact Us</a></li>
+                                   
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="filmology.php"><i class="icon-calendar"></i>Filmology</a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="blog.php"><i class="icon-calendar"></i>Blog</a>
+                                
+                            </li>
+                            
+                           <li>
+                                <a href="community.php"><i class="icon-calendar"></i>Community</a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="contact.php"><i class="icon-calendar"></i>Contact</a>
+                                
+                            </li>
+                            
+                            
+                        </ul>
+                    </div>
+                    <!-- END NAV -->     
+                </div>
+            </div>
+            <!-- END HEADER -->
+
+            <!-- PAGE META -->
+			<div id="page_meta" class="home-section">
+			    <div class="inner group">
+			        <div class="meta-left">
+			            <h2 class="page-title">Community</h2>
+			        </div>
+			        <div class="meta-right">
+			            <p id="crumbs" class="theme_breadcumb"><a class="home" href="index.php">Home</a> //  <a class="no-link current" href="community.php">Community</a></p>
+			        </div>
+			    </div>
+			</div>
+			<!-- /PAGE META -->        
+			
+			<div id="primary" class="layout-sidebar-right home-section">
+			    <div class="inner group">
+			        <!-- START CONTENT -->
+			        <div id="content" class="group">
+			            <div class="page type-page status-publish hentry group">
+			                <h2 class="title-post-page">Media</h2>
+			                <h3>You Tube Video</h3>
+			                <div class="post_video youtube"><iframe width="560" height="315" src="https://www.youtube.com/embed/PMs77x0TQIA" frameborder="0" allowfullscreen></iframe></div>
+			                <br>
+
+			                <div class="post_video youtube"><iframe width="560" height="315" src="https://www.youtube.com/embed/5A_8IcKaM9k" frameborder="0" allowfullscreen></iframe></div>
+			                
+			                
+			                
+			                
+			               
+			                <h2>Socials Media</h2>
+			                <a href="#" class="socials facebook" style="font-size:45px;" title="Facebook">F</a>
+			                <a href="#" class="socials twitter" style="font-size:45px;" title="Twitter">L</a>
+			                <a href="#" class="socials skype" style="font-size:45px;" title="Skype">H</a>
+			                <a href="#" class="socials google" style="font-size:45px;" title="Google">G</a>
+			                
+			            </div>
+			            <div id="comments">
+			                <!--<p class="nocomments">&nbsp;</p>-->
+			            </div>
+			            <!-- #comments -->
+			        </div>
+			        <!-- END CONTENT -->
+			        <!-- START SIDEBAR -->
+			        
+			        </div>
+			        <!-- END SIDEBAR -->    
+			        <!-- START EXTRA CONTENT -->
+			        
+			            
+			            
+			            <div class="one-third last">
+			                <h3>Google Map</h3>
+			                <div class="google-map-frame">
+			                    <iframe width="500" height="272" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.it/maps?f=q&amp;source=s_q&amp;hl=it&amp;geocode=&amp;q=catania&amp;aq=&amp;sll=41.442726,12.392578&amp;sspn=18.129097,39.506836&amp;ie=UTF8&amp;hq=&amp;hnear=Catania,+Sicilia&amp;z=12&amp;output=embed"></iframe>
+			                    <div class="shadow-thumb-sidebar"></div>
+			                </div>
+			                <br />
+			                
+			                <div class="clear"></div>
+			            </div>
+			            <div class="clear"></div>
+			            <br />
+			            <hr />
+			            
+			            
+			            
+			        </div>
+			        <!-- END EXTRA CONTENT -->
+			    </div>
+			</div>
+			<div class="clear"></div>
+		</div>     
+		<!-- END WRAPPER -->
+		
+		<div id="footer" class="sidebar-right">
+            <div class="group inner footer_row_1 footer_cols_3">
+                
+                <div class="widget-first widget recent-posts">
+                    <h3>From our blog</h3>
+                    <div class="recent-post group">
+                        
+                        <div class="hentry-post group">
+                             <?php
+                include("includes/connect.php");
+            
+                $query = "select * from blog order by 1 DESC LIMIT 0,3";
+            
+                $run =mysqli_query($con,$query);
+            
+            while ($row=mysqli_fetch_array($run)) {
+            
+                $blog_id =$row['blog_id'];
+                $title =$row['blog_title'];
+                $blog_date =$row['blog_date'];
+                $image =$row['blog_image'];
+               
+                ?>
+                <a href="blogpages.php?id=<?php echo $blog_id; ?>">
+                       
+                 <h3 class="title"><?php echo $title; ?></h3></a>
+                 <p><b><?php echo $blog_date; ?></b></p>
+                  
+                   
+            
+               
+                                
+                                 <?php } ?>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div class="widget widget_nav_menu">
+                    <h3>Custom menu</h3>
+                    <div class="menu-footer-menu-container">
+                        <ul id="menu-footer-menu" class="menu">
+                            <li class="columns2"><a href="blog.php">Blog</a></li>
+                            <li class="columns2"><a href="news.php">News</a></li>
+                            <li class="columns2"><a href="filmology.php">Filmology</a></li>
+                            <li class="columns2"><a href="contact.php">Contact</a></li>
+                            <li class="columns2"><a href="awards.php">Awards</a></li>
+                            <li class="columns2"><a href="gallery.php">Gallery</a></li>
+                            <li class="columns2"><a href="community.php">Community</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="quick-contact-widget widget two-third last">
+                    <div class="widget-last widget quick-contact">
+                        <h3>Quick Contact</h3>
+                        <form id="contact-form-example" class="contact-form" method="post" action="sendmail.php" enctype="multipart/form-data">
+                            <div class="usermessagea"></div>
+                            <fieldset>
+                                <ul>
+                                    <li class="text-field">
+                                        <label for="name-example">
+                                        <span class="label">Name</span>
+                                        </label>
+                                        <div class="input-prepend"><span class="add-on"><i class="icon-user"></i></span><input type="text" name="name" id="name-example" class="required" value="" /></div>
+                                        <div class="msg-error"></div>
+                                    </li>
+                                    <li class="text-field">
+                                        <label for="email-example">
+                                        <span class="label">Email</span>
+                                        </label>
+                                        <div class="input-prepend"><span class="add-on"><i class="icon-envelope"></i></span><input type="text" name="email" id="email-example" class="required email-validate" value="" /></div>
+                                        <div class="msg-error"></div>
+                                    </li>
+                                    <li class="textarea-field">
+                                        <label for="message-example">
+                                        <span class="label">Message</span>
+                                        </label>
+                                        <div class="input-prepend"><span class="add-on"><i class="icon-pencil"></i></span><textarea name="message" id="message-example" rows="8" cols="30" class="required"></textarea></div>
+                                        <div class="msg-error"></div>
+                                    </li>
+                                    <li class="submit-button">
+                                        <input type="text" name="yiw_bot" id="yiw_bot" />
+                                        <input type="hidden" name="action" value="sendemail" id="action" />
+                                        <input type="hidden" name="yiw_referer" value="#" />
+                                        <input type="hidden" name="id_form" value="footer" />
+                                        <input type="submit" name="yiw_sendemail" value="send message" class="sendmail alignright" />			
+                                    </li>
+                                </ul>
+                            </fieldset>
+                        </form>
+                        <script type="text/javascript">
+                            var messages_form_footer = {
+                            	name: "Insert your name",
+                            	email: "Insert your email",
+                            	message: "Insert your message"
+                            };
+                        </script>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+
+        <!-- START FOOTER -->
+        <div id="copyright" class="group">
+            <div class="inner group">
+                <div class="left">
+                    <p>
+                    	Copyright  @2016 | <a href="#"><strong>USAMAMUKWAYA</strong></a> | All Rights Reserved
+                    </p>
+                </div>
+                <div class="right">
+                    <a href="#" class="socials facebook" style="font-size:30px;" title="Facebook">F</a>
+                    <a href="#" class="socials twitter" style="font-size:30px;" title="Twitter">L</a>
+                    <a href="#" class="socials skype" style="font-size:30px;" title="Skype">H</a>
+                    <a href="#" class="socials google" style="font-size:30px;" title="Google">G</a>
+                    <a href="#" class="socials linkedin" style="font-size:30px;" title="Linkedin">I</a>
+                    <a href="#" class="socials rss" style="font-size:30px;" title="Rss">R</a>
+                </div>
+            </div>
+        </div>
+        
+        <script type="text/javascript" src="js/jquery.custom.js"></script>
+        <script type="text/javascript" src="js/contact.js"></script>
+        
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36516261-21']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>		
+    </body>
+</html>
