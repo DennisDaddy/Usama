410776fbd4c239cf58e99aa161af052fa2983352
DennisDaddy <softteez95@gmail.com>
2016-11-24 02:27:37 -0800

    add footer copyright

--- gallery.php
+++ gallery.php
@@ -224,7 +224,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    &copy; 2016 | All Rights Reserved.
+                     &copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved.
                 </div>
                 <div class="col-sm-6">
                     <ul class="pull-right">
