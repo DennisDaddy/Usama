6bcff6daf8be4b252d5f2ec1b53063f39484e3ed
DennisDaddy <softteez95@gmail.com>
2016-11-24 02:20:25 -0800

    add images and replace images page

--- gallery.php
+++ gallery.php
@@ -15,7 +15,11 @@
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-     <link rel="shortcut icon" href="images/ico/us.jpg">
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
 <body>
     <header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
@@ -27,24 +31,22 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php"><h1 style="color: #D4AF37;" >Usama Mukwaya</h1></h1></a>
+                <a class="navbar-brand" href="index.php"><h2>Usama Mukwaya</h2></a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                    <li><a href="filmology.php"><i class="fa fa-film" aria-hidden="true"></i> Filmology</a></li>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="filmology.php">Filmology</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i> Osam <i class="icon-angle-down"></i></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Osam <i class="icon-angle-down"></i></a>
                         <ul class="dropdown-menu">
-                           <li><a href="news.php">Osam</a></li>
                            <li><a href="news.php">News</a></li>
                            <li><a href="gallery.php">Gallery</a></li>
                            <li><a href="awards.php">Awards</a></li>
                         </ul>
                     </li>
-                    <li><a href="blog.php"><i class="fa fa-pencil" aria-hidden="true"></i> Blog</a></li> 
-                    <li><a href="community.php"><i class="fa fa-users" aria-hidden="true"></i> Community</a></li> 
-                    <li><a href="contact.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
+                    <li><a href="blog.php">Blog</a></li> 
+                    <li><a href="contact.php">Contact</a></li>
                 </ul>
             </div>
         </div>
@@ -68,37 +70,35 @@
 
     <section id="portfolio" class="container">
         
-        <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
-
-      <div class="col-md-3 step">
-      <a href="images/1.jpg"><img src="images/1.jpg" data-lity class="img-thumbnail" /></a>        
-      </div>
+        <ul class="portfolio-items col-3">
+            <li class="portfolio-item apps">
+                <div class="item-inner">
+                    <img src="images/portfolio/thumb/1.jpg" width="50" alt="">
+                    
+                    <div class="overlay">
+                        <a class="preview btn btn-danger" href="images/portfolio/full/1.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>             
+                    </div>           
+                </div>           
+            </li><!--/.portfolio-item-->
+            <li class="portfolio-item joomla bootstrap">
+                <div class="item-inner">
+                    <img src="images/portfolio/thumb/2.jpg" alt="">
+                  
+                    <div class="overlay">
+                        <a class="preview btn btn-danger" href="images/portfolio/full/2.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>              
+                    </div>           
+                </div>           
+            </li><!--/.portfolio-item-->
+            <li class="portfolio-item bootstrap wordpress">
+                <div class="item-inner">
+                    <img src="images/portfolio/thumb/3.jpg" alt="">
+                   
+                    <div class="overlay">
+                        <a class="preview btn btn-danger" href="images/portfolio/full/3.jpg" rel="prettyPhoto"><i class="icon-eye-open"></i></a>        
+                    </div>           
+                </div>           
+            </li><!--/.portfolio-item-->
+        </ul>
     </section><!--/#portfolio-->
 
     <section id="bottom" class="wet-asphalt">
@@ -226,21 +226,21 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                     &copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved.
+                    &copy; 2016 | All Rights Reserved.
                 </div>
                 <div class="col-sm-6">
                     <ul class="pull-right">
-                        <li><a href="https://www.facebook.com/OnlineUsama"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-                        <li><a href="https://twitter.com/usamamukwaya"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="https://www.youtube.com/user/TheUgmovies"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-                        <li><a href="https://plus.google.com/+UsamaMukwaya"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/OnlineUsama">Facebook</a></li>
+                        <li><a href="https://twitter.com/usamamukwaya">Twitter</a></li>
+                        <li><a href="https://www.youtube.com/user/TheUgmovies">Youtube</a></li>
+                        <li><a href="https://plus.google.com/+UsamaMukwaya">Google Plus</a></li>
                         <li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li><!--#gototop-->
                     </ul>
                 </div>
             </div>
         </div>
     </footer><!--/#footer-->
-    <script src="https://use.fontawesome.com/4d0c79038f.js"></script>
+
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.prettyPhoto.js"></script>
