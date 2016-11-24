a3f5799fa0d847e5ffaea99c506ce52f803bdd36
DennisDaddy <softteez95@gmail.com>
2016-11-24 01:26:00 -0800

    remove content

--- osam.php
+++ osam.php
@@ -71,35 +71,7 @@
     <section id="privacy-policy" class="container">
      <img  src="images/blog/boo.jpg" width="100%" height="500" />
         <h3>Bala Bala Sese      2015</h3>
-        <p>Natasha Sinayobye, Micheal Kasaija, Raymond Rushabiro, Ashraf Ssemwogere<br>
-        
-
-        An incredible story of boyfriend's battle for love through perseverance. In the outskirts of Sese Island, John (Micheal Kasaija) is madly in love with Maggie and both are willing to take their love forward. Facing abuses and harassments by malicious Maggie's father Kasirivu (Raymond Rushabiro), John, helped by his young brother Alex (Ssesanga Ismael) is determined to take in all but to retain the love of his life especially when he finds out that he has a contender village tycoon (Jabal Dungu) who is also lining up for Maggie.
-        </p>
-        
-
-        <p>&nbsp;</p>
-
-        <h3>In Just Hours       2013</h3>
-        <p>Ssentongo Isaac as Peter, Allen Musumba, Veronica Nakayo, Shafique Ssenyange<br>
-
-        Peter is about to be surprised by a mysterious, crude condition from the disease he has been suffering from. Weirdly the disease he is suffering from is only about to get fatal in the next few hours and if not treated quickly and carefully, he might not be sexually productive again.
-        </p>
-
-        <p>&nbsp;</p>
-
-        <h3>She Likes Prada     2011</h3>
-        <p>Diana Kahunde, Ankur Rathee, Kato Lubega, Allen Komujini<br>
-
-            Wealthy Felix is married to Grace of the outstanding gorgeousness. To Grace, the matrimony is not a commitment but just only a road to her needs and benefits. She loves the Mercedes Benz.  She is more in love with shopping, good looks and anything trendy better than him especially her favorite label, Prada. She is so prepared to spend every penny Felix makes even with Daisy’s (Grace’s best friend) confrontations. Daisy too has alot more in common with her friend including changing boyfriends like nothing and now having a new one Jessie she is replacing with David who is taking her on Holiday to Mombasa.  When Felix announces that he has been diagnosed with liver cancer which has metastasized, he’s told that he could die if he doesn’t get enough or early treatment and Grace is a little not surprised even for the fact that he is going to die. Comes in Felix’s doctor; capable John, who has away-out for him through chemotherapy but it’s risky and very expensive that Grace won’t allow all the money to go just like that however important it might be for Felix. She has a lot to spend the money to like looking after herself-say breast fragmentation for his husband’s happiness than his unreliable liver cancer which may kill him if not treated in the proper description. However expensive the treatment is, she partners with attractive Doctor John to help her husband in a way that not even Felix can ever imagine. Taking in on the treatment is risky for Felix and so is for Grace’s beauty to be skipped by John! Grace succeeds in luring Dr John to do the treatment. She finally not only likes Prada, but she adores Dr Felix and still has alot to save from her soon dying husband’s fortune.  </p>
-
-        <p>&nbsp;</p>
-        <h3>Hello        2010</h3>
-        <p>Kahunda Laura, John-Martin Ntabaazi, Katushabe Swiam<br>
-            A countryside world of jealousy and possessiveness. A cheerful and passionately devoted couple of Kakumba and Rehma is introduced to the fanciful crazy world of telephone technology but this never goes well to what the phone might bring. After finding Rehma sweet talking on the phone on her farming grounds, Kakumba madly believes that her wife is cheating on him with some other man. A ruckus that is started by Kakumba involves in their neighbor Teresa who tries to make peace between the fighting couple but just as it aggravates, Kakumba receives a call from his niece.  Rehma can’t understand Kakumba’s flattery behaviors on the phone especially with the presence of her mordant neighbor Teresa who can’t seize emphasizing on what Kakumba has just done to Rehma.
-            </p>
-
-        <p>&nbsp;</p>
+    
         <h3>Pain of Lies        2010</h3>
         <p>Faridah Kuteesa, Joshua Nyombi, Leila Nasanga, Usama Mukwaya, Ronald Karamuzi<br>
 
