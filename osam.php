9722a488459360009fda15b5a3eedc2ae3bbc39a
DennisDaddy <softteez95@gmail.com>
2016-11-24 01:21:41 -0800

    create osam page

---
+++ osam.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>UsamaMukwaya | Filmology</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+     <link rel="shortcut icon" href="images/ico/us.jpg">
+</head><!--/head-->
+<body>
+    <header class="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><h1 style="color: #D4AF37;" >Usama Mukwaya</h1></h1></a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                    <li><a href="filmology.php"><i class="fa fa-film" aria-hidden="true"></i> Filmology</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i> Osam <i class="icon-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                           <li><a href="news.php">News</a></li>
+                           <li><a href="gallery.php">Gallery</a></li>
+                           <li><a href="awards.php">Awards</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="blog.php"><i class="fa fa-pencil" aria-hidden="true"></i> Blog</a></li> 
+                    <li><a href="community.php"><i class="fa fa-users" aria-hidden="true"></i> Community</a></li> 
+                    <li><a href="contact.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header><!--/header-->
+
+    <section id="title" class="emerald">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>Filmology</h1>
+                    
+                </div>
+                <div class="col-sm-6">
+                    <ul class="breadcrumb pull-right">
+                        <li><a href="index.php">Home</a></li>
+                       
+                        <li class="active">Filmology</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section><!--/#title--> 
+
+    <section id="privacy-policy" class="container">
+     <img  src="images/blog/boo.jpg" width="100%" height="500" />
+        <h3>Bala Bala Sese      2015</h3>
+        <p>Natasha Sinayobye, Micheal Kasaija, Raymond Rushabiro, Ashraf Ssemwogere<br>
+        
+
+        An incredible story of boyfriend's battle for love through perseverance. In the outskirts of Sese Island, John (Micheal Kasaija) is madly in love with Maggie and both are willing to take their love forward. Facing abuses and harassments by malicious Maggie's father Kasirivu (Raymond Rushabiro), John, helped by his young brother Alex (Ssesanga Ismael) is determined to take in all but to retain the love of his life especially when he finds out that he has a contender village tycoon (Jabal Dungu) who is also lining up for Maggie.
+        </p>
+        
+
+        <p>&nbsp;</p>
+
+        <h3>In Just Hours       2013</h3>
+        <p>Ssentongo Isaac as Peter, Allen Musumba, Veronica Nakayo, Shafique Ssenyange<br>
+
+        Peter is about to be surprised by a mysterious, crude condition from the disease he has been suffering from. Weirdly the disease he is suffering from is only about to get fatal in the next few hours and if not treated quickly and carefully, he might not be sexually productive again.
+        </p>
+
+        <p>&nbsp;</p>
+
+        <h3>She Likes Prada     2011</h3>
+        <p>Diana Kahunde, Ankur Rathee, Kato Lubega, Allen Komujini<br>
+
+            Wealthy Felix is married to Grace of the outstanding gorgeousness. To Grace, the matrimony is not a commitment but just only a road to her needs and benefits. She loves the Mercedes Benz.  She is more in love with shopping, good looks and anything trendy better than him especially her favorite label, Prada. She is so prepared to spend every penny Felix makes even with Daisy’s (Grace’s best friend) confrontations. Daisy too has alot more in common with her friend including changing boyfriends like nothing and now having a new one Jessie she is replacing with David who is taking her on Holiday to Mombasa.  When Felix announces that he has been diagnosed with liver cancer which has metastasized, he’s told that he could die if he doesn’t get enough or early treatment and Grace is a little not surprised even for the fact that he is going to die. Comes in Felix’s doctor; capable John, who has away-out for him through chemotherapy but it’s risky and very expensive that Grace won’t allow all the money to go just like that however important it might be for Felix. She has a lot to spend the money to like looking after herself-say breast fragmentation for his husband’s happiness than his unreliable liver cancer which may kill him if not treated in the proper description. However expensive the treatment is, she partners with attractive Doctor John to help her husband in a way that not even Felix can ever imagine. Taking in on the treatment is risky for Felix and so is for Grace’s beauty to be skipped by John! Grace succeeds in luring Dr John to do the treatment. She finally not only likes Prada, but she adores Dr Felix and still has alot to save from her soon dying husband’s fortune.  </p>
+
+        <p>&nbsp;</p>
+        <h3>Hello        2010</h3>
+        <p>Kahunda Laura, John-Martin Ntabaazi, Katushabe Swiam<br>
+            A countryside world of jealousy and possessiveness. A cheerful and passionately devoted couple of Kakumba and Rehma is introduced to the fanciful crazy world of telephone technology but this never goes well to what the phone might bring. After finding Rehma sweet talking on the phone on her farming grounds, Kakumba madly believes that her wife is cheating on him with some other man. A ruckus that is started by Kakumba involves in their neighbor Teresa who tries to make peace between the fighting couple but just as it aggravates, Kakumba receives a call from his niece.  Rehma can’t understand Kakumba’s flattery behaviors on the phone especially with the presence of her mordant neighbor Teresa who can’t seize emphasizing on what Kakumba has just done to Rehma.
+            </p>
+
+        <p>&nbsp;</p>
+        <h3>Pain of Lies        2010</h3>
+        <p>Faridah Kuteesa, Joshua Nyombi, Leila Nasanga, Usama Mukwaya, Ronald Karamuzi<br>
+
+Babirye, an adopted but raised a beloved twin daughter bends the story short when she twirls against her family's possessions and life including snatching her sister's hunky boyfriend Mac. The whole plot is brought to an end by the unexpected return of her brother Jason from the States and the strange disappearance of her sister Nakato. The truth is yet to be revealed in this never told tale.
+</p>
+        <p>&nbsp;</p>
+    </section><!--/#privacy-policy-->
+
+    <section id="bottom" class="wet-asphalt">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <h4>Latest News</h4>
+                     <div>
+                        <div class="media">
+                            <div class="pull-left">
+                                
+                            </div>
+                            <div class="media-body">
+                                <?php
+                include("includes/connect.php");
+            
+                $query = "select * from blog order by 1 DESC LIMIT 0,3";
+            
+                $run =mysqli_query($con,$query);
+            
+            while ($row=mysqli_fetch_array($run)) {
+            
+                $blog_id =$row['blog_id'];
+                $title =$row['blog_title'];
+                $blog_date =$row['blog_date'];
+                $image =$row['blog_image'];
+               
+                ?>
+                <a href="blogpages.php?id=<?php echo $blog_id; ?>">
+                       
+               <span class="media-heading"><?php echo $title; ?></span></a>
+                 <p><b><?php echo $blog_date; ?></b></p>
+                  
+                   
+            
+               
+                                
+                                 <?php } ?>
+                            
+                            </div>
+                        </div>
+                        
+                        
+                    </div>  
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Custom Menu</h4>
+                    <div>
+                        <ul class="arrow">
+                            <li><a href="filmology.php">Filmology</a></li>
+                            <li><a href="news.php">News</a></li>
+                            <li><a href="awards.php">Awards</a></li>
+                             <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="blog.php">Blog</a></li>
+                            <li><a href="community.php">Community</a></li>
+                            <li><a href="contact.php">Contact Us</a></li>
+                           
+                        </ul>
+                    </div>
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Latest Blog</h4>
+                    <div>
+                        <div class="media">
+                            <div class="pull-left">
+                                
+                                
+                            </div>
+                            <div class="media-body">
+
+                                <?php
+                include("includes/connect.php");
+            
+                $query = "select * from blog order by 1 DESC LIMIT 0,3";
+            
+                $run =mysqli_query($con,$query);
+            
+            while ($row=mysqli_fetch_array($run)) {
+            
+                $blog_id =$row['blog_id'];
+                $title =$row['blog_title'];
+                $blog_date =$row['blog_date'];
+                $image =$row['blog_image'];
+               
+                ?>
+                <a href="blogpages.php?id=<?php echo $blog_id; ?>">
+                       
+                 <span class="media-heading"><?php echo $title; ?></span></a>
+                 <p><b><?php echo $blog_date; ?></b></p>
+                  
+                   
+            
+               
+                                
+                                 <?php } ?>
+                            </div>
+                        </div>
+                    </div>  
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Address</h4>
+                    <address>
+                         <br>
+                        <br>
+                        <br>
+                    </address>
+                    <h4>Newsletter</h4>
+                    <form role="form">
+                        <div class="input-group">
+                            <input type="text" class="form-control" autocomplete="off" placeholder="Enter your email">
+                            <span class="input-group-btn">
+                                <button class="btn btn-danger" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </form>
+                </div> <!--/.col-md-3-->
+            </div>
+        </div>
+    </section><!--/#bottom-->
+
+    <footer id="footer" class="midnight-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                     &copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved.
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                       <li><a href="https://www.facebook.com/OnlineUsama"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                        <li><a href="https://twitter.com/usamamukwaya"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.youtube.com/user/TheUgmovies"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                        <li><a href="https://plus.google.com/+UsamaMukwaya"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li><!--#gototop-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+    <script src="https://use.fontawesome.com/4d0c79038f.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
