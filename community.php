6693c4ff74f6bc7aa4d7ad49b439afb09b903bbc
DennisDaddy <softteez95@gmail.com>
2016-11-25 05:30:39 -0800

    add font color

--- community.php
+++ community.php
@@ -32,7 +32,7 @@
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                    <li><a href="filmology.php"><i class="fa fa-film" aria-hidden="true"></i> Community</a></li>
+                    <li><a href="filmology.php"><i class="fa fa-film" aria-hidden="true"></i> Filmology</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i> Osam <i class="icon-angle-down"></i></a>
                         <ul class="dropdown-menu">
@@ -85,7 +85,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3 col-sm-6">
-                    <h4>Latest News</h4>
+                    <h4 style="color: #D4AF37;">Latest News</h4>
                      <div>
                         <div class="media">
                             <div class="pull-left">
@@ -126,7 +126,7 @@
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
-                    <h4>Custom Menu</h4>
+                    <h4 style="color: #D4AF37;">Custom Menu</h4>
                     <div>
                         <ul class="arrow">
                             <li><a href="filmology.php">Filmology</a></li>
@@ -142,7 +142,7 @@
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
-                    <h4>Latest Blog</h4>
+                    <h4 style="color: #D4AF37;">Latest Blog</h4>
                     <div>
                         <div class="media">
                             <div class="pull-left">
@@ -182,13 +182,13 @@
                 </div><!--/.col-md-3-->
 
                 <div class="col-md-3 col-sm-6">
-                    <h4>Address</h4>
+                    <h4 style="color: #D4AF37;">Address</h4>
                     <address>
-                         <br>
+                        <br>
                         <br>
                         <br>
                     </address>
-                    <h4>Newsletter</h4>
+                    <h4 style="color: #D4AF37;">Newsletter</h4>
                     <form role="form">
                         <div class="input-group">
                             <input type="text" class="form-control" autocomplete="off" placeholder="Enter your email">
@@ -206,11 +206,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                   <p>&copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved. |  <a href="admin/login.php"><i class="icon-lock"></i>  Admin Login</a></p>
+                    <p style="color: #D4AF37;">&copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved. |  <a href="admin/login.php"><i class="icon-lock"></i>  Admin Login</a></p>
                 </div>
                 <div class="col-sm-6">
                     <ul class="pull-right">
-                       <li><a href="https://www.facebook.com/OnlineUsama"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/OnlineUsama"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
                         <li><a href="https://twitter.com/usamamukwaya"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
                         <li><a href="https://www.youtube.com/user/TheUgmovies"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
                         <li><a href="https://plus.google.com/+UsamaMukwaya"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
