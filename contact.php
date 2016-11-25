1d63dbe01dcf31ec7f85d5f8fa6dcfe2bc13e7f3
DennisDaddy <softteez95@gmail.com>
2016-11-25 04:44:27 -0800

    add font family

--- contact.php
+++ contact.php
@@ -70,7 +70,7 @@
     <section id="contact-page" class="container">
         <div class="row">
             <div class="col-sm-8">
-                <h4>Contact Form</h4>
+                <h4 style="color: #d9534f; font-family: cursive;">Contact Form</h4>
                 <div class="status alert alert-success" style="display: none"></div>
                 <form id="main-contact-form" class="contact-form" name="contact-form" method="post" action="sendemail.php" role="form">
                     <div class="row">
@@ -96,7 +96,7 @@
                     
             </div><!--/.col-sm-8-->
             <div class="col-sm-4">
-                <h4>Our Location</h4>
+                <h4 style="color: #d9534f; font-family: cursive;">Our Location</h4>
                 <iframe width="100%" height="215" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127672.26884318159!2d32.52908433918015!3d0.3140102822760062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbc0f9d74b39b%3A0x4538903dd96b6fec!2sKampala!5e0!3m2!1sen!2sug!4v1479852255915"></iframe>
             </div><!--/.col-sm-4-->
         </div>
