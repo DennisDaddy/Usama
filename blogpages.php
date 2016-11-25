4a90911e9136874dbbe1eb63fa70ae6238632832
DennisDaddy <softteez95@gmail.com>
2016-11-25 05:38:19 -0800

    add font color

--- blogpages.php
+++ blogpages.php
@@ -63,13 +63,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <h1>Blog</h1>
+                    <h1 style="color: #D4AF37;">Blog</h1>
                    
                 </div>
                 <div class="col-sm-6">
                     <ul class="breadcrumb pull-right">
-                        <li><a href="index.php">Home</a></li>
-                        <li class="active">Blog</li>
+                        <li><a style="color: #D4AF37;" href="index.php">Home</a></li>
+                        <li style="color: #D4AF37;" class="active">Blog</li>
                     </ul>
                 </div>
             </div>
