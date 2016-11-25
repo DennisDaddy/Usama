cf3795627b147c5f56e91c3810e5a4e5e2beb9a3
DennisDaddy <softteez95@gmail.com>
2016-11-25 00:54:03 -0800

    add footer admin login link

--- news.php
+++ news.php
@@ -307,7 +307,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    &copy; 2016 | All Rights Reserved.
+                   <p>&copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved. |  <a href="admin/login.php"><i class="icon-lock"></i>  Admin Login</a></p>
                 </div>
                 <div class="col-sm-6">
                     <ul class="pull-right">
