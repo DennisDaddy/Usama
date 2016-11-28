3d20eddd628dc148d5564e6740de333c26af51fb
DennisDaddy <softteez95@gmail.com>
2016-11-28 14:26:34 -0800

    change font color

--- news.php
+++ news.php
@@ -71,8 +71,8 @@
                 </div>
                 <div class="col-sm-6">
                     <ul class="breadcrumb pull-right">
-                        <li><a style="color: #D4AF37;" href="index.php">Home</a></li>
-                        <li style="color: #D4AF37;" class="active">News</li>
+                        <li><a  href="index.php">Home</a></li>
+                        <li  class="active">News</li>
                     </ul>
                 </div>
             </div>
