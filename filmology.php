af81ba52149b929f667c818544c8994fa6cfa281
DennisDaddy <softteez95@gmail.com>
2016-11-28 14:20:55 -0800

    change font color

--- filmology.php
+++ filmology.php
@@ -59,9 +59,9 @@
                 </div>
                 <div class="col-sm-6">
                     <ul class="breadcrumb pull-right">
-                        <li><a style="color: #D4AF37;" href="index.php">Home</a></li>
+                        <li><a  href="index.php">Home</a></li>
                        
-                        <li style="color: #D4AF37;" class="active">Filmology</li>
+                        <li  class="active">Filmology</li>
                     </ul>
                 </div>
             </div>
