2f157191550ba8f5a176bf3f11da57cf0aa4ce52
DennisDaddy <softteez95@gmail.com>
2016-11-28 10:16:15 -0800

    send data

--- admin/registration.php
+++ admin/registration.php
@@ -1,3 +1,8 @@
+
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -111,3 +116,27 @@
     <script src="js/main.js"></script>
 </body>
 </html>
+<?php
+include("includes/connect.php");
+if (isset($_POST['login'])) {
+    # code...
+    $user_name=$_POST['user_name'];
+    $user_pass=$_POST['user_pass'];
+
+    $admin_query ="select * from admin_login where user_name='$user_name' AND user_pass='$user_pass'";
+
+    $run =mysqli_query($con,$admin_query);
+    if (mysqli_num_rows($run)>0) {
+        # code...
+        $_SESSION['user_name'] =$user_name;
+
+        echo "<script>window.open('index.php','_self')</script>";
+    }
+    else{
+
+        echo "<script>alert('username or password incorrect!')</script>";
+    }
+}
+
+
+?>
