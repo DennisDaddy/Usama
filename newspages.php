d94b17763cea1f6ef25834f0e7c9fc229c20844b
DennisDaddy <softteez95@gmail.com>
2016-11-29 14:01:26 +0300

    add thumbnail stylings

--- newspages.php
+++ newspages.php
@@ -165,7 +165,7 @@
                 ?>
                 <a href="blogpages.php?id=<?php echo $blog_id; ?>">
                  <p><b><?php echo $title; ?></b></p></a>
-                           <div class="thumbnail home-thumb">
+                           <div id="single_post">
                                 <img width="150" height="100" src='images2/uploads/<?php echo $image; ?>' >
                                 </div>
                                        
