b2d802db5215a81f929a155685cf7daa8b1dc483
DennisDaddy <softteez95@gmail.com>
2016-11-28 16:41:12 -0800

    change video width

--- community.php
+++ community.php
@@ -72,11 +72,11 @@
      
         <h3>Bala Bala Sese </h3>
         
-                            <div><iframe width="60%" height="450" src="https://www.youtube.com/embed/PMs77x0TQIA" frameborder="0" allowfullscreen></iframe></div>
+                            <div><iframe width="100%" height="450" src="https://www.youtube.com/embed/PMs77x0TQIA" frameborder="0" allowfullscreen></iframe></div>
                             <br>
                             <hr>
                           <h3>Tales From EDYAC</h3>
-                            <div class="post_video youtube"><iframe width="60%" height="450" src="https://www.youtube.com/embed/5A_8IcKaM9k" frameborder="0" allowfullscreen></iframe></div>
+                            <div class="post_video youtube"><iframe width="100%" height="450" src="https://www.youtube.com/embed/5A_8IcKaM9k" frameborder="0" allowfullscreen></iframe></div>
                 <hr>
             <p>&nbsp;</p>
 
