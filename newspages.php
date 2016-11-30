62609370135188704cf3120931bde771cf50c474
DennisDaddy <softteez95@gmail.com>
2016-11-30 14:30:44 +0300

    replace awards  and gallery

--- newspages.php
+++ newspages.php
@@ -47,8 +47,8 @@
                         <ul class="dropdown-menu">
                            <li><a href="osam.php"><i class="fa fa-user" aria-hidden="true"></i> Osam</a></li>
                            <li><a href="news.php"><i class="fa fa-newspaper-o" aria-hidden="true"></i> Press</a></li>
-                           <li><a href="gallery.php"><i class="fa fa-picture-o" aria-hidden="true"></i> Gallery</a></li>
                            <li><a href="awards.php"><i class="fa fa-gift" aria-hidden="true"></i> Awards</a></li>
+                           <li><a href="gallery.php"><i class="fa fa-picture-o" aria-hidden="true"></i> Gallery</a></li>
                         </ul>
                     </li>
                     <li><a href="blog.php"><i class="fa fa-pencil" aria-hidden="true"></i> Blog</a></li> 
