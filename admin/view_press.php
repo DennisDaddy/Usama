9ff6134a279bebce20fd4bdeded2c98979f04f76
DennisDaddy <softteez95@gmail.com>
2016-11-30 09:14:05 +0300

    add insert press link

--- admin/view_press.php
+++ admin/view_press.php
@@ -13,7 +13,7 @@
 <div id="sidebar">
 	<h2><a href="logout.php">Logout</a></h2>
 	<h2><a href="view_press.php">View Press</a></h2>
-	<h2><a href="index.php?insert=insert">Add Press</a></h2>
+	<h2><a href="insert_press.php">Add Press</a></h2>
 	
 </div> 
 <table width="1000" border="3" align="center" bgcolor="pink">
