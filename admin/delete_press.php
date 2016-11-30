38fcd3f84d514eb4c46b8f15b5da877f75bcc02b
DennisDaddy <softteez95@gmail.com>
2016-11-30 09:16:11 +0300

    enable press deletion

--- admin/delete_press.php
+++ admin/delete_press.php
@@ -4,13 +4,13 @@ include("includes/connect.php");
 if (isset($_GET['del'])) {
 	# code...
 	$delete_id = $_GET['del'];
-	$delete_query ="delete from posts where post_id='$delete_id' ";
+	$delete_query ="delete from press where post_id='$delete_id' ";
 
 	if (mysqli_query($con,$delete_query)) {
 		# code..
-		echo "<script> alert('post has been Deleted') </script>";
+		echo "<script> alert('Press has been Deleted Successfully') </script>";
 		echo 
-		"<script> window.open('view_posts.php','_self') </script>";
+		"<script> window.open('view_press.php','_self') </script>";
 	}
 
 }
