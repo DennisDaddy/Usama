5af4d4323a9c00cac20cb70b706e432f8fb7f35b
DennisDaddy <softteez95@gmail.com>
2016-11-30 14:00:04 +0300

    replace footer

--- osam.php
+++ osam.php
@@ -230,7 +230,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                     <p>&copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved. |  <a href="admin/login.php"><i class="icon-lock"></i>  Admin Login</a></p>
+                     <p>All Rights Reserved | Usam Mukwaya  &copy; <?php echo date("Y"); ?> |  <a href="admin/login.php"><i class="icon-lock"></i>  Admin Login</a></p>
 
                 </div>
                 <div class="col-sm-6">
