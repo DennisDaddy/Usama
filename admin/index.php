aa9d4017393341eab76a5bee91737e91661d2235
DennisDaddy <softteez95@gmail.com>
2016-12-01 15:40:12 +0300

    add admin

--- admin/index.php
+++ admin/index.php
@@ -1,45 +1,256 @@
-<?php 
-session_start();
-
-if (!isset($_SESSION['user_name'])) {
-	# code...
-	header("location:login.php");
-}
-
-
- ?>
-
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Admin Panel</title>
-	<link rel="stylesheet" type="text/css" href="admin_style.css" />
-</head>
-<body>
-<div id="header">
-<a href="index.php"><h1>Welcome to the Admin Panel</h1></a>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+    <meta name="author" content="GeeksLabs">
+    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+    <link rel="shortcut icon" href="img/favicon.png">
 
-</div>
+    <title>Admin Area</title>
 
-<div id="sidebar">
-	<h2><a href="logout.php">Logout</a></h2>
-	<h2><a href="view_posts.php">View News</a></h2>
-	<h2><a href="insert_post.php">Add News</a></h2>
-	<h2><a href="insert_blog.php">Add Blog post</a></h2>
-	<h2><a href="view_blog.php">View Blog Posts</a></h2>
-	
-</div> 
-<div id="welcome">
-<h1 style="color:black" >Welcome to your Admin Panel</h1>
-<p style="color:black">This is your admin panel where you can manage your website files and content </p>
-</div>
-<?php
-if (isset($_GET['insert'] )) {
-	# code...
-	include("insert_post.php");
-}
+    <!-- Bootstrap CSS -->    
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- bootstrap theme -->
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <!--external css-->
+    <!-- font icon -->
+    <link href="css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" />    
+    <!-- full calendar css-->
+    <link href="assets/fullcalendar/fullcalendar/bootstrap-fullcalendar.css" rel="stylesheet" />
+	<link href="assets/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" />
+    <!-- easy pie chart-->
+    <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/>
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+	<link href="css/jquery-jvectormap-1.2.2.css" rel="stylesheet">
+    <!-- Custom styles -->
+	<link rel="stylesheet" href="css/fullcalendar.css">
+	<link href="css/widgets.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+	<link href="css/xcharts.min.css" rel=" stylesheet">	
+	<link href="css/jquery-ui-1.10.4.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+      <script src="js/lte-ie7.js"></script>
+    <![endif]-->
+  </head>
 
-?>
+  <body>
+  <!-- container section start -->
+  <section id="container" class="">
+     
+      
+      <header class="header dark-bg">
+            <div class="toggle-nav">
+                <div class="icon-reorder tooltips" data-original-title="Toggle Navigation" data-placement="bottom"><i class="icon_menu"></i></div>
+            </div>
 
-</body>
+            <!--logo start-->
+            <a href="index.php" class="logo">Admin<span class="lite">Area</span></a>
+            <!--logo end-->
+
+            <div class="nav search-row" id="top_menu">
+                               
+            </div>
+
+            <div class="top-nav notification-row">                
+                <!-- notificatoin dropdown start-->
+                <ul class="nav pull-right top-menu">
+                    
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="profile-ava">
+                                <img alt="" src="img/avatar3.png">
+                            </span>
+                            <span class="username">Admin</span>
+                            <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu extended logout">
+                            <div class="log-arrow-up"></div>                          
+                    
+                            <li>
+                                <a href="login.php"><i class="icon_key_alt"></i> Log Out</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <!-- user login dropdown end -->
+                </ul>
+                <!-- notificatoin dropdown end-->
+            </div>
+      </header>      
+      <!--header end-->
+
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu">                
+                  <li class="active">
+                      <a class="" href="index.php">
+                          <i class="icon_house_alt"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+				  <li class="sub-menu">
+                      <a href="view_posts.php" class="">
+                          <i class="fa fa-eye" aria-hidden="true"></i>
+                          <span>View News</span>
+                          </a>                      
+                  </li>       
+                  <li class="sub-menu">
+                      <a href="insert_post.php" class="">
+                          <i class="fa fa-plus" aria-hidden="true"></i>
+                          <span>Add News</span>
+                         
+                      </a>
+                      
+                  </li>
+                  <li>
+                      <a class="" href="insert_blog.php">
+                          <i class="fa fa-plus" aria-hidden="true"></i>
+                          <span>Add Blog Posts</span>
+                      </a>
+                  </li>
+                  <li>                     
+                      <a class="" href="view_blog.php">
+                          <i class="fa fa-eye" aria-hidden="true"></i>
+                          <span>View Blog Post</span>
+                          
+                      </a>
+                                         
+                  </li>
+                             
+                  <li class="sub-menu">
+                      <a href="logout.php" class="">
+                          <i class="fa fa-sign-out" aria-hidden="true"></i>
+                          <span>Logout</span>
+                          
+                      </a>
+                     
+                  </li>                  
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
+      
+      <!--main content start-->
+      <section id="main-content">
+      <section class="wrapper">            
+              <!--overview start-->
+        <div class="row">
+        <div class="col-lg-12">
+          <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+          <ol class="breadcrumb">
+            <li><i class="fa fa-home"></i><a href="index.php">Home</a></li>
+            <li><i class="fa fa-laptop"></i>Dashboard</li>                
+          </ol>
+          <h2 align="center" >Welcome to your Admin Panel</h2>
+          <p align="center">This is your admin panel where you can manage your website files and content </p>
+        </div>
+      </div>
+          
+      </section>
+      <!--main content end-->
+  </section>
+  <!-- container section start -->
+
+    <!-- javascripts -->
+    <script src="js/jquery.js"></script>
+	<script src="js/jquery-ui-1.10.4.min.js"></script>
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- nice scroll -->
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <!-- charts scripts -->
+    <script src="assets/jquery-knob/js/jquery.knob.js"></script>
+    <script src="js/jquery.sparkline.js" type="text/javascript"></script>
+    <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
+    <script src="js/owl.carousel.js" ></script>
+    <!-- jQuery full calendar -->
+    <<script src="js/fullcalendar.min.js"></script> <!-- Full Google Calendar - Calendar -->
+	<script src="assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
+    <!--script for this page only-->
+    <script src="js/calendar-custom.js"></script>
+	<script src="js/jquery.rateit.min.js"></script>
+    <!-- custom select -->
+    <script src="js/jquery.customSelect.min.js" ></script>
+	<script src="assets/chart-master/Chart.js"></script>
+   
+    <!--custome script for all page-->
+    <script src="js/scripts.js"></script>
+    <!-- custom script for this page-->
+    <script src="js/sparkline-chart.js"></script>
+    <script src="js/easy-pie-chart.js"></script>
+	<script src="js/jquery-jvectormap-1.2.2.min.js"></script>
+	<script src="js/jquery-jvectormap-world-mill-en.js"></script>
+	<script src="js/xcharts.min.js"></script>
+	<script src="js/jquery.autosize.min.js"></script>
+	<script src="js/jquery.placeholder.min.js"></script>
+	<script src="js/gdp-data.js"></script>	
+	<script src="js/morris.min.js"></script>
+	<script src="js/sparklines.js"></script>	
+	<script src="js/charts.js"></script>
+	<script src="js/jquery.slimscroll.min.js"></script>
+  <script>
+
+      //knob
+      $(function() {
+        $(".knob").knob({
+          'draw' : function () { 
+            $(this.i).val(this.cv + '%')
+          }
+        })
+      });
+
+      //carousel
+      $(document).ready(function() {
+          $("#owl-slider").owlCarousel({
+              navigation : true,
+              slideSpeed : 300,
+              paginationSpeed : 400,
+              singleItem : true
+
+          });
+      });
+
+      //custom select box
+
+      $(function(){
+          $('select.styled').customSelect();
+      });
+	  
+	  /* ---------- Map ---------- */
+	$(function(){
+	  $('#map').vectorMap({
+	    map: 'world_mill_en',
+	    series: {
+	      regions: [{
+	        values: gdpData,
+	        scale: ['#000', '#000'],
+	        normalizeFunction: 'polynomial'
+	      }]
+	    },
+		backgroundColor: '#eef3f7',
+	    onLabelShow: function(e, el, code){
+	      el.html(el.html()+' (GDP - '+gdpData[code]+')');
+	    }
+	  });
+	});
+
+
+
+  </script>
+
+  </body>
 </html>
--- admin/login.php
+++ admin/login.php
@@ -1,121 +1,67 @@
-
 <?php
-session_start();
+ session_start();
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Admin Login | Usama Mukwaya</title>
+    <meta name="description" content="Creative - Bootstrap 3 Responsive Admin Template">
+    <meta name="author" content="GeeksLabs">
+    <meta name="keyword" content="Creative, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
+    <link rel="shortcut icon" href="img/favicon.png">
+
+    <title>Admin Login</title>
+
+    <!-- Bootstrap CSS -->    
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+    <!-- bootstrap theme -->
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <!--external css-->
+    <!-- font icon -->
+    <link href="css/elegant-icons-style.css" rel="stylesheet" />
+    <link href="css/font-awesome.css" rel="stylesheet" />
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 -->
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
-    <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/us.jpg">
-</head><!--/head-->
-<body>
-    <header class="navbar navbar-inverse navbar-fixed-top midnight-blue" role="banner">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php"><h1 style="color: #D4AF37; font-family: cursive;" >Usama Mukwaya</h1></a>
-            </div>
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="../index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                    <li><a href="../filmology.php"><i class="fa fa-film" aria-hidden="true"></i> Filmology</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i> Osam <i class="icon-angle-down"></i></a>
-                        <ul class="dropdown-menu">
-                           <li><a href="../osam.php"><i class="fa fa-user" aria-hidden="true"></i> Osam</a></li>
-                           <li><a href="../news.php"><i class="fa fa-newspaper-o" aria-hidden="true"></i> News</a></li>
-                           <li><a href="../gallery.php"><i class="fa fa-picture-o" aria-hidden="true"></i> Gallery</a></li>
-                           <li><a href="../awards.php"><i class="fa fa-gift" aria-hidden="true"></i> Awards</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="../blog.php"><i class="fa fa-pencil" aria-hidden="true"></i> Blog</a></li> 
-                    <li><a href="../community.php"><i class="fa fa-users" aria-hidden="true"></i> Community</a></li> 
-                    <li><a href="../contact.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    </header><!--/header-->
+    <![endif]-->
+</head>
+<body class="login-img3-body">
 
-    <section id="title" class="emerald">
-        <div class="container">
-            <div class="row">
-                
-                
-            </div>
-        </div>
-    </section><!--/#title-->     
+    <div class="container">
 
-    <section id="registration" class="container">
-     <h1 align="center">Admin Login Area</h1>
-        <form  class="center" role="form" action="login.php" method="post" >
-            <fieldset class="registration-form">
-                <div class="form-group">
-                    <input type="text" id="username" name="user_name" placeholder="Username" class="form-control">
-                </div>
-                
-                <div class="form-group">
-                    <input type="password" id="password" name="user_pass" placeholder="Password" class="form-control">
-                </div>
-                
-                <div class="form-group">
-                    <button name="login" class="btn btn-success btn-md btn-block">Login</button>
-                </div>
-            </fieldset>
-        </form>
-    </section><!--/#registration-->
 
-    <section id="bottom" class="wet-asphalt">
-        <div class="container">
-          
-            </div>
-        </div>
-    </section><!--/#bottom-->
+      <form class="login-form" action="login.php"  method="post"> 
 
-    
-    <footer id="footer" class="midnight-blue">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                     <p style="color: #D4AF37;">&copy; <?php echo date("Y"); ?> | Usam Mukwaya |  All Rights Reserved.</p>
-                </div>
-                <div class="col-sm-6">
-                    <ul class="pull-right">
-                       <li><a href="https://www.facebook.com/OnlineUsama"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
-                        <li><a href="https://twitter.com/usamamukwaya"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="https://www.youtube.com/user/TheUgmovies"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-                        <li><a href="https://plus.google.com/+UsamaMukwaya"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-                        <li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li><!--#gototop-->
-                    </ul>
-                </div>
+        <div class="login-wrap">
+            <p class="login-img"><i class="icon_lock_alt"></i></p>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="icon_profile"></i></span>
+              <input type="text" class="form-control" name="user_name" placeholder="Username" autofocus>
             </div>
+            <div class="input-group">
+
+                <span class="input-group-addon"><i class="icon_key_alt"></i></span>
+                <input type="password" name="user_pass" class="form-control" placeholder="Password">
+            </div>
+            
+            <button class="btn btn-primary btn-lg btn-block" name="login" type="submit">Admin Login</button>
+            
         </div>
-    </footer><!--/#footer-->
-    <script src="https://use.fontawesome.com/4d0c79038f.js"></script>
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/main.js"></script>
-</body>
+      </form>
+
+    </div>
+
+
+  </body>
 </html>
+
 <?php
 include("includes/connect.php");
 if (isset($_POST['login'])) {
@@ -138,5 +84,3 @@ if (isset($_POST['login'])) {
     }
 }
 
-
-?>
