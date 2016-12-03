a760899a3c41e2df07ebf36dcfdc32f3d4d3612f
DennisDaddy <softteez95@gmail.com>
2016-12-03 14:27:58 +0300

    add press links

--- admin/view_press.php
+++ admin/view_press.php
@@ -126,6 +126,21 @@
                       </a>
                                          
                   </li>
+
+                  <li>
+                      <a class="" href="insert_blog.php">
+                          <i class="fa fa-plus" aria-hidden="true"></i>
+                          <span>Add Press</span>
+                      </a>
+                  </li>
+                  <li>                     
+                      <a class="" href="view_blog.php">
+                          <i class="fa fa-eye" aria-hidden="true"></i>
+                          <span>View Press</span>
+                          
+                      </a>
+                                         
+                  </li>
                              
                   <li class="sub-menu">
                       <a href="logout.php" class="">
