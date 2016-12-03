7e0974a4983776fc9f46dae57b7107d8c8dfb2b0
DennisDaddy <softteez95@gmail.com>
2016-12-03 12:20:00 +0300

    add news page

--- news.php
+++ news.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Welcome to Usama Mukwaya Official website  award winning Ugandan filmmaker, He is a Ugandan film director, screenwriter, producer, actor, and television host.">
     <meta name="author" content="">
-    <title>UsamaMukwaya | Press</title>
+    <title>UsamaMukwaya | News</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
